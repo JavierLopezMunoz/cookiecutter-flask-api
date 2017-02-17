55dfdd6bba8a5bc83d56c207c00c00932383d4a7
Javier Lopez <javier@lopezmunoz.name>
2017-02-17 14:53:20 +0100

    Remove coding: utf-8

--- {{cookiecutter.app_name}}/tests/test_config.py
+++ {{cookiecutter.app_name}}/tests/test_config.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """Test configs."""
 from {{cookiecutter.app_name}}.app import create_app
 from {{cookiecutter.app_name}}.settings import DevelopmentConfig, ProductionConfig
